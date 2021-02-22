8d8228823220f0eeac387388b6560628388d9b47
choiyeonseong <am3251@naver.com>
2021-02-22 17:27:30 +0900

    4장 커밋 0222

---
+++ chap04/Chap04App/IncDecTestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26;
+            Console.WriteLine($"현재 숫자는 {result}");
+
+            result += 3;
+            Console.WriteLine($"현재 숫자는 {result}");
+
+            result -= 10;
+            Console.WriteLine($"현재 숫자는 {result}");
+
+            Console.WriteLine($"후치 증가 값은 {result++}");    // 후치 증가 연산
+            Console.WriteLine($"전치 증가 값은 {++result}");    // 전치 증가 연산
+
+            Console.WriteLine($"전치 감소 값은 {--result}");    // 전치 감소 연산
+            Console.WriteLine($"후치 감소 값은 {result--}");    // 후치 감소 연산
+            Console.WriteLine($"현재 숫자는 {result}");
+        }
+    }
+}
