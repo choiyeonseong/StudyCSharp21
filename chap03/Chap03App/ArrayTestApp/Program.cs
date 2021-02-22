fd02e152de61336f74e05fd46f170363afbb4aca
choiyeonseong <am3251@naver.com>
2021-02-22 15:15:54 +0900

    3장 중간 커밋

--- chap03/Chap03App/ArrayTestApp/Program.cs
+++ chap03/Chap03App/ArrayTestApp/Program.cs
@@ -6,7 +6,18 @@ namespace ArrayTestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("배열테스트");
+
+            int[] arr = new int[10];
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
         }
     }
 }
