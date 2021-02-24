f714a8c2b6691373a840deae252ad3888b48c009
choiyeonseong <am3251@naver.com>
2021-02-24 10:42:04 +0900

    0224 6장 완료 커밋

---
+++ chap06/Chap06App/OverLoadTestApp/Calculator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadTestApp
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----계산기-----");
+
+            Console.Write("#int\t");
+            int x = Calculator.Plus(3, 4);
+            Console.WriteLine($"3 + 4 = {x}");
+
+            Console.Write("#float\t");
+            float y = Calculator.Plus(3.14f, 5.6f);
+            Console.WriteLine($"3.14 + 5.6 = {y}");
+
+            Console.Write("#double\t");
+            Console.WriteLine($"3.14 + 5.6 = {Calculator.Plus(3.14, 5.6)}");
+
+            Console.Write("#string\t");
+            Console.WriteLine($"3.14 + 5.6 = {Calculator.Plus(3.14, "5")}");
+
+            Console.WriteLine("#add parameter");
+            int z = Calculator.Plus(3, 4, 5);
+            Console.WriteLine($"3 + 4 + 5 = {z}");
+
+            Console.WriteLine("#가변길이 매개변수");
+            Console.WriteLine($"10까지의 합은 {Sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}");
+
+            int[] arrs = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine($"10까지 배열의 합은 {Sum(arrs)}");
+
+        }
+
+        // 가변길이 매개 변수 - Using params
+        private static int Sum(params int[] args)
+        {
+            int result = 0;
+
+            foreach (var arg in args)
+            {
+                result += arg;
+            }
+
+            return result;
+        }
+
+        private static int Sum(int v1, int v2)
+        {
+            int result = v1 + v2;
+            return result;
+        }
+
+        private static double Plus(double v1, string v2)
+        {
+            double.TryParse(v2, out double p2);
+            double result = v1 + p2;
+            return result;
+        }
+
+        // return type이 object - return type이 뭔지 몰라서 -> Boxing
+        private static /*object*/ double Plus(double v1, double v2)
+        {
+            double result = v1 + v2;
+            return result;
+        }
+
+        // Overloading - 이름만 같고 매개변수의 type은 다름
+        private static float Plus(float v1, float v2)
+        {
+            float result = v1 + v2;
+            return result;
+        }
+
+        // Parameter 개수 추가
+        private static int Plus(int v1, int v2, int v3)
+        {
+            int result = v1 + v2 + v3;
+            return result;
+        }
+
+        private static int Plus(int v1, int v2)
+        {
+            int result = v1 + v2;
+            return result;
+        }
+
+        # region 불가능 - 매개변수 개수도 같고 타입도 같으면 Overloading 불가능
+        /*
+        private static long Plus(int v1, int v2)
+        {
+            long result = v1 + v2;
+            return result;
+        }
+
+        private static int Plus(float v1, float v2)
+        {
+            int result = (int)(v1 + v2);
+            return result;
+        }
+        */
+        #endregion
+    }
+}
--- chap06/Chap06App/ProfileTestApp/Program.cs
+++ chap06/Chap06App/ProfileTestApp/Program.cs
@@ -12,18 +12,32 @@ namespace ProfileTestApp
         {
             PrintProfile("최연성", "010-3901-3464");
 
-            PrintProfile(null, "333-4444-55555");
+            if (PrintProfile(null, "333-4444-55555") == -1)
+            {
+                Console.WriteLine("*출력시 오류 발생*");
+            }
+            
+            // 명명된 매개변수 - 가독성, 순서변경 가능, 정확한 명시
+            PrintProfile(phone: "010-9999-9999", name: "홍길동");
+
+            // 선택적 매개변수 - defalt값 설정 가능, 모호함 발생
+            PrintProfile("홍기동");
+            //PrintProfile("010-2325-3464");
+            PrintProfile(phone: "010-2325-3464");
         }
 
-        public static void PrintProfile(string name, string phone)
+        public static int PrintProfile(string name = "홍길동", string phone = "010-7979-7979")
+        // public static int PrintProfile(string name, string phone = "010-7979-7979)  //가능 
+        // public static int PrintProfile(string name = "홍길동", string phone)        //불가능 - method선언시 뒤에있는 prameter부터 선택적 매개변수 설정 가능
         {
             if (string.IsNullOrEmpty(name)) // IsNullOrEmpty : null이거나 비었으면 True
             {
                 Console.WriteLine("이름을 정확히 입력하세요!");
-                return;
+                return -1;
             }
             // 프로필 출력
             Console.WriteLine($"이름 : {name}\n번호 : {phone}");
+            return 0;
         }
     }
 }
