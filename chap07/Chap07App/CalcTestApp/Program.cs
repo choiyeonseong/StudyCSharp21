8683e1cdc6e995d1339609ce0ab09040c1117987
choiyeonseong <am3251@naver.com>
2021-02-24 12:40:29 +0900

    0224 점심시간

---
+++ chap07/Chap07App/CalcTestApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcTestApp
+{
+    class Mathz
+    {
+        // 여러 행위(기능 : 메서드) 들이 많다...를 표현한것이래
+        public int Plus(int a, int b)
+        {
+            //...
+            return 1;
+        }
+
+        public void PrintEven(int val)
+        {
+            if (val % 2 == 0)
+            {
+                Console.WriteLine($"짝수 : {val}");
+            }
+            else Console.WriteLine($"홀수 : {val}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //....
+
+            Mathz mathz = new Mathz();  // instance 객체화
+
+            int x;
+
+            //...
+
+            x = 5;
+            /*if (x % 2 == 0)
+            {
+                Console.WriteLine($"x의 값 : {x}");
+            }*/
+            mathz.PrintEven(x);
+
+            //...
+
+            x = 10;
+            /*if (x % 2 == 0)
+            {
+                Console.WriteLine($"x의 값 : {x}");
+            }*/
+            mathz.PrintEven(x);
+        }
+    }
+}
---
+++ chap07/Chap07App/Chap07App/ObjectTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Chap07App
+{
+    class ObjectTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("객체 사용");
+
+            // 객체의 선언, 객체의 실체화
+            Cat cat1 = new Cat();   // 고양이 객체의 실체(instance) 생성
+            // 속성
+            cat1.Name = "야통이";
+            cat1.Color = "고등어";
+            // 기능
+            cat1.Meow();
+
+            Cat cat2 = new Cat();
+            cat2.Name = "삼색이";
+            cat2.Color = "삼색";
+            cat2.Meow();
+
+            Cat cat3 = new Cat();
+            cat3.Color = "고등어";
+            cat3.Name = "무";
+            cat3.Meow();
+
+            Cat cat4 = new Cat("래기", "고등어");
+            cat4.Meow();
+            Cat cat5 = new Cat(_name: "Marilyn", _color: "젖소");
+            cat5.Meow();
+            Cat cat6 = new Cat("연님", "고등어", "암컷");
+            cat6.Meow();
+        }
+    }
+
+    // 객체, 클래스
+    class Cat
+    {
+        // 기본 생성자 - defalut값 (안 만들어도 쓸 수 있음)
+        public Cat() { }
+
+        //~Cat() { } // 더이상 필요없음
+
+        // 사용자 지정 생성자 - 기본 생성자를 만들어야 쓸 수 있음
+        public Cat(string _name, string _color)
+        {
+            Name = _name;
+            Color = _color;
+        }
+
+        // 생성자 오버로딩
+        public Cat(string _name, string _color, string _gender)
+        {
+            Name = _name;
+            Color = _color;
+            Gender = _gender;
+        }
+
+        // 특성, 속성, 변수
+        public string Name;
+        public string Color;
+        public string Gender;
+
+        // 행위, 기능, 메소드
+        public void Meow()
+        {
+            if (Color == "고등어")
+                Console.WriteLine($"{Name} : 이얏호응");
+            else if(Color == "삼색")
+                Console.WriteLine($"{Name} : 삐약");
+            else
+                Console.WriteLine($"{Name} : 엙옹");
+        }
+    }
+}
