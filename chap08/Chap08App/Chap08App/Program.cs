14d703469e4689cb34b5bd5cb9df9c84ea9a35a0
choiyeonseong <am3251@naver.com>
2021-02-25 15:22:52 +0900

    0225 8장 인터페이스

---
+++ chap08/Chap08App/Chap08App/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap08App
+{
+    interface ILogger
+    {
+        void WriteLog(string msg);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString()} log : {msg}");
+        }
+    }
+
+    class CustomLogger : ILogger
+    {
+        public void WriteLog(string msg)
+        {
+            Console.WriteLine($"Log : {msg}\a\a");  // \a : 알림소리
+        }
+    }
+
+    class ClimateLogger
+    {
+        private ILogger logger;
+        public ClimateLogger(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                Console.Write("온도를 입력 : ");
+                string temp = Console.ReadLine();   //
+                if (string.IsNullOrEmpty(temp)) break;
+
+                logger.WriteLog($"현재온도 : {temp}");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //ILogger logger = new ILogger();       // 인터페이스의 인스턴스는 만들 수 없음
+                ILogger logger = new ConsoleLogger(); // 가능(consolelogger가 ilogger을 상속받았기 때문에)
+                //ConsoleLogger logger = new ConsoleLogger();
+                logger.WriteLog("로그 출력");   // 직접 제어
+
+                ClimateLogger clogger = new ClimateLogger(new ConsoleLogger());
+                clogger.Start();    // 제어 역전, 역흐름0(Inversion of Control(IOC))
+
+               /* ClimateLogger clogger = new ClimateLogger(new CustomLogger()); 
+                clogger.Start();    // 제어 역전, 역흐름0(Inversion of Control(IOC))*/
+            }
+        }
+    }
+}
---
+++ chap08/Chap08App/MultiInterfaceApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MultiInterfaceApp
+{
+    interface IRunnable     // 자동차 종류
+    {
+        void Run();
+    }
+
+    interface IFlyable      // 비행기 종류
+    {
+        void Fly();
+    }
+
+    class DroneCar : IRunnable, IFlyable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Run!!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("Fly!!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DroneCar");
+
+            DroneCar dreamCar = new DroneCar();
+            dreamCar.Run();
+            dreamCar.Fly();
+
+            Console.WriteLine("차로 변경(interface)");
+            IRunnable car = dreamCar;
+            car.Run();
+            //car.Fly();    // 오류
+
+            Console.WriteLine("비행기로 변경(interface)");
+            IFlyable plane = dreamCar;
+            plane.Fly();
+            //plane.Run();    // 오류
+        }
+    }
+}
