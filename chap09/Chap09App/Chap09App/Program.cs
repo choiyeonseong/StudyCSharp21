bd8e935097fe80eccfc4906a749b1c0160a7f739
choiyeonseong <am3251@naver.com>
2021-02-25 16:48:08 +0900

    0225 9장 프로퍼티

--- chap09/Chap09App/Chap09App/Program.cs
+++ chap09/Chap09App/Chap09App/Program.cs
@@ -6,10 +6,70 @@ using System.Threading.Tasks;
 
 namespace Chap09App
 {
+    class MyClass
+    {
+        private int iValue;   // 0~120℃ 값만 입력받는 멤버변수
+        private double dZeta;
+        private float fPng;
+        private string strVal;
+        private int inCode;
+
+        public int IValue // 프로퍼티 : getter, setter를 동시에 선언하고 method호출없이 바로사용가능(읽기전용인경우 get만 선언)
+        {     
+            get { return this.iValue; }
+            set 
+            {
+                if (value < 0)
+                    this.iValue = 0;
+                else if (value > 120)
+                    this.iValue = 120;
+                else
+                    this.iValue = value;
+            }
+        }
+
+        public MyClass(int iValue)   // 생성자에도 setter이용해서 조건 만족 초기화
+        {
+            // this.SetValue(iValue);
+            IValue = iValue;
+        }
+        /*
+                public void SetValue(int iValue)
+                {
+                    if (iValue < 0)
+                    {
+                        this.iValue = 0;
+                    }
+                    else if (iValue > 120)
+                    {
+                        this.iValue = 120;
+                    }
+                    else
+                    {
+                        this.iValue = iValue;
+                    }
+                }
+                public int GetValue()
+                {
+                    return this.iValue;
+                }*/
+        public void PrintValue()
+        {
+            Console.WriteLine($"값은 {this.iValue}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            MyClass myClass = new MyClass(1200);
+            myClass.PrintValue();
+
+            //myClass.SetValue(108);
+            myClass.IValue = 108;   // set, get method를 입력하지 않고 set, get 기능 사용가능
+            myClass.PrintValue();
+            //Console.WriteLine($"현재 온도는 {myClass.GetValue()}℃ 입니다");
+            Console.WriteLine($"현재 온도는 {myClass.IValue}℃ 입니다");
         }
     }
 }
---
+++ chap09/Chap09App/PropertyTestApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTestApp
+{
+    //생일 정보 클래스
+    class BirthdayInfo
+    {
+        private string name;
+        public DateTime birthday;
+
+        //Property
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }  //value : 프로퍼티에서 사용, 무조건 value
+        }
+        public DateTime Birthday
+        {
+            get { return this.birthday; }
+            set { this.birthday = value; }
+        }
+
+        //Getter,Setter
+        public string GetName()
+        {
+            return this.name;
+        }
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public DateTime GetBirthday()
+        {
+            return this.birthday;
+        }
+        public void SetBirthday(DateTime birthday)
+        {
+            this.birthday = birthday;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("일반 get,set 메서드 사용");
+
+            BirthdayInfo info = new BirthdayInfo();
+            info.SetName("홍길동");    // 메서드처럼 값을 입력
+            info.SetBirthday(new DateTime(1999, 1, 8));
+
+            Console.WriteLine($"오늘은 {info.GetBirthday()}!!\n" +
+                              $"{info.GetName()}의 생일을 축하합니다!!");
+
+            Console.WriteLine("프로퍼티 사용");
+
+            BirthdayInfo info2 = new BirthdayInfo();
+            info2.Name = "홍길순"; // 멤버 변수에 접근하는 것 처럼 값을 입력
+            info2.Birthday = new DateTime(1992, 8, 19);
+
+            Console.WriteLine($"오늘은 {info2.Birthday}!!\n" +
+                              $"{info2.Name}의 생일을 축하합니다!!");
+        }
+    }
+}
