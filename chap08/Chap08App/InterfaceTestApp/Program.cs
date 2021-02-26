a9f2c1a1d2771b01f7012902e3ee6ea7b792891e
choiyeonseong <am3251@naver.com>
2021-02-26 15:47:22 +0900

    0226 8장 인터페이스 추가

---
+++ chap08/Chap08App/InterfaceTestApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTestApp
+{
+    /*    class Ridable   // 탈것
+        {
+            public virtual void Ride()
+            {
+                Console.WriteLine("탈것!");
+            }
+        }*/
+
+    interface ICar
+    {
+        void Run();
+        void Ride();
+    }
+
+    interface IPlane
+    {
+        void Fly();
+        void Ride();
+    }
+
+    class DroneCar : IPlane, ICar
+    {
+        public void Fly()
+        {
+            Console.WriteLine("DroneCar Fly!");
+        }
+
+        public void Ride()  // 내용은 없고 구현만 하면 됨 -> 다중상속 문제 해결
+        {
+            Run();
+            Fly();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("DroneCar Run!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DroneCar dreamCar = new DroneCar();
+            dreamCar.Ride();
+        }
+    }
+}
---
+++ chap08/Chap08App/LoggerTestApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerTestApp
+{
+    interface ILogger
+    {
+        void WriteLog(string message);
+    }
+
+    class ConsoleLogger : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine($"{DateTime.Now} : {message}");
+        }
+    }
+
+    interface IFormattableLogger : ILogger  // 인터페이스 상속 (클래스에 쓰면 구현, 하위 인터페이스에 쓰면 상속)
+    {
+        void WriteLog(string format, params Object[] args);
+    }
+
+    class ConsoleFormatLogger : IFormattableLogger
+    {
+        public void WriteLog(string format, params object[] args)
+        {
+            string message = string.Format(format, args);//?
+            Console.WriteLine($"{DateTime.Now.ToLocalTime()} / {message}");
+        }
+
+        public void WriteLog(string message)
+        {
+            Console.WriteLine($"{DateTime.Now.ToLocalTime()} / {message}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ConsoleLogger 테스트");
+            ConsoleLogger logger = new ConsoleLogger();
+            logger.WriteLog("로그 메세지");
+
+            IFormattableLogger logger2 = new ConsoleFormatLogger();
+            logger2.WriteLog("{0} * {1} = {2}", 3, 4, 12);  //IFormattableLogger.WriteLog(format, args)
+        }
+    }
+}
