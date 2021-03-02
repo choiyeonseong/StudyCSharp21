42a7308810af53349dfb13e24bcadd528546fa0c
choiyeonseong <am3251@naver.com>
2021-03-02 17:24:58 +0900

    0302 13장 대리자 체인까지

---
+++ chap13/Chap13App/CallbackTestApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallbackTestApp
+{
+    delegate int Compare(int a, int b); // a, b 비교 대리자
+    class Program
+    {
+        // 오름차 순 : a > b -> 1, a==b -> 0, a < b -> -1
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b) return 1;    //a, b 바꿈
+            else if (a == b) return 0;
+            else return -1;
+        }
+        // 내림차 순 : a < b -> 1, a==b -> 0, a > b -> -1
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b) return 1;    //a, b 바꿈
+            else if (a == b) return 0;
+            else return -1;
+        }
+        static void BubbleSort(int[] DataSet, Compare comparer)
+        {
+            int temp = 0;
+            for (int i = 0; i < DataSet.Length; i++){
+                for (int j = 0; j < DataSet.Length - (i + 1); j++){
+                    // 비교하여 값 위치 변경 swap
+                    if (comparer(DataSet[j], DataSet[j+1]) > 0)
+                    {
+                        temp = DataSet[j + 1];
+                        DataSet[j + 1] = DataSet[j];
+                        DataSet[j] = temp;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 8, 4, 2, 1, 10 };
+            
+            Console.WriteLine("Sorting......");
+            // 버블 정렬 오름차순
+            BubbleSort(array, new Compare(AscendCompare));
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            
+            Console.WriteLine("Sorting......");
+            // 버블 정렬 내림차순
+            BubbleSort(array, new Compare(DescendCompare));
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ chap13/Chap13App/DelegateChainApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegateChainApp
+{
+    class Program
+    {
+        delegate void AllCalc(int x, int y);    // 대리자 선언
+
+        static void Plus(int a, int b) { Console.WriteLine($"a + b = {a + b}"); }
+        static void Minus(int a, int b) { Console.WriteLine($"a - b = {a - b}"); }
+        static void Multiple(int a, int b) { Console.WriteLine($"a * b = {a * b}"); }
+        static void Divide(int a, int b) { Console.WriteLine($"a / b = {a / b}"); }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculating!!");
+
+            //Plus(10, 5);
+            //Minus(10, 5);
+            //Multiple(10, 5);
+            //Divide(10, 5);
+
+            // 대리자 체인 연결
+            AllCalc allCalc = Plus;
+            allCalc += Minus;
+            allCalc += Multiple;
+            allCalc += Divide;
+            allCalc(10, 5); // 하나의 호출로 여러가지 일을 한꺼번에 처리할 수 있음 (winform에서 많이 사용)
+
+            Console.WriteLine("\n곱셈 메서드 제거");
+            allCalc -= Multiple;
+            allCalc(10, 5); // 곱셉 메소드 제거 후 실행
+        }
+    }
+}
